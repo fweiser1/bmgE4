f4ed19eb2f88c62bb67e47371d31c95deead6957
Administrateur <Administrateur@6212-D.cite-schuman.edu>
2018-11-20 16:50:40 +0100

    Début gestion auteur + _ref.lib terminé

--- include/_reference.lib.php
+++ include/_reference.lib.php
@@ -58,4 +58,46 @@ class Genre {
 
 }
 
+class Auteur {
+    private $_id;
+    private $_nom;
+    private $_prenom;
+    private $_alias;
+    private $_notes;
+    
+    public function __construct(
+            $p_id,
+            $p_nom,
+            $p_prenom,
+            $p_alias,
+            $p_notes
+    ) {
+        $this->setID($p_id);
+        $this->setNom($p_code);
+        $this->setPrenom($p_libelle);
+        $this->setAlias($p_libelle);
+        $this->setNotes($p_libelle);
+    }  
+    
+    public function getID() {
+        return $this->_id;
+    }
+    
+    public function getNom() {
+        return $this->_nom;
+    }
+    
+    public function getPrenom() {
+        return $this->_prenom;
+    }
+    
+    public function getAlias() {
+        return $this->_alias;
+    }
+    
+    public function getNotes() {
+        return $this->_notes;
+    }
+}
+
 
