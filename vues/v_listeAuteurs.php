741e5616e3fb378ef232c258dcc99dde45e40719
Administrateur <Administrateur@6212-C.cite-schuman.edu>
2018-11-27 14:46:41 +0100

    Fin v_listeGenres

--- vues/v_listeAuteurs.php
+++ vues/v_listeAuteurs.php
@@ -1,7 +1,6 @@
-<?php
-///////////////////////////////////////////////// Baptiste ////////////////////////////////////////////////////
+<?php // COUCOU c'est FLOOOOOOOOOWWW
 /**
- * Page de gestion des auteurs
+ * Page de gestion des genres
 
  * @author 
  * @package default
@@ -13,46 +12,47 @@ require_once 'include/_data.lib.php';
 ?>
 
 <div id="content">
-    <h2>Gestion des auteurs</h2>
+    <h2>Gestion des Auteurs</h2>
     <a href="index.php?uc=gererAuteurs&action=ajouterAuteur" title="Ajouter">
         Ajouter un auteur
     </a>
     <div class="corps-form">
-        <!--- afficher la liste des auteurs -->
-        <fieldset>	
+        <!--- afficher la liste des genres -->
+        <fieldset>        
             <legend>Auteurs</legend>
             <div id="object-list">
                 <?php
-//                $cnx = connectDB();
-//                // récupérer les auteurs
-//                $strSQL = "SELECT id_auteur as ID, "
-//                        . " nom AS Nom "
-//                        . "FROM v_auteurs ";
-//                $lesAuteurs = getRows($cnx, $strSQL);
-//                // afficher le nombre de auteurs      
-//                $nbAuteurs = $lesAuteurs->rowCount();
                 echo '<span>' . $nbAuteurs . ' auteur(s) trouvé(s)'
                 . '</span><br /><br />';
-                // afficher un tableau des auteurs
+                // afficher un tableau des genres
                 if ($nbAuteurs > 0) {
                     // création du tableau
                     echo '<table>';
                     // affichage de l'entete du tableau 
-                    echo '<tr><th>ID</th><th>Nom</th></tr>';
+                    echo '<tr>';
+                    // création entete tableau avec noms de colonnes  
+                    echo '<th>Id</th>';
+                    echo '<th>Nom</th>';
+                    echo '</tr>';
                     // affichage des lignes du tableau
                     $n = 0;
                     foreach ($lesAuteurs as $unAuteur) {
-//                    while ($ligne = $lesAuteurs->fetch()) {
                         if (($n % 2) == 1) {
                             echo '<tr class="impair">';
                         } else {
                             echo '<tr class="pair">';
                         }
                         // afficher la colonne 1 dans un hyperlien
-                        echo '<td><a href="afficherAuteur.php?id='
-                        . $ligne[0] . '">' . $ligne[0] . '</a></td>';
+                        echo '<td><a href="index.php?uc=gererAuteurs&action=consulterAuteur&id='
+                        . $unAuteur->getId().'">' . $unAuteur->getId().'</a></td>';
                         // afficher les colonnes suivantes
-                        echo '<td>' . $ligne[1] . '</td>';
+                    echo '<td>' . $unAuteur->getNom()." ".$unAuteur->getPrenom();
+                           // " (".$unAuteur->getAlias().")" . '</td>';
+                           if($unAuteur->getAlias() != NULL)
+                           {
+                               echo " (".$unAuteur->getAlias().")";
+                           }
+                           echo '</td>';
                         echo '</tr>';
                         $n++;
                     }
@@ -60,10 +60,8 @@ require_once 'include/_data.lib.php';
                 } else {
                     echo "Aucun auteur trouvé !";
                 }
-//                $lesAuteurs->closeCursor();
-//                disconnectDB($cnx);
                 ?>
             </div>
         </fieldset>
     </div>
-</div>
+</div> 
